125c09360a30ab6c578b4eee1af0219b1b384f07
Jonathan Philpott <jonphilpott@users.noreply.github.com>
2019-09-25 11:31:42 -0600

    blah

--- main.c
+++ main.c
@@ -24,18 +24,30 @@
 
 /*{pal:"nes",layout:"nes"}*/
 const char PALETTE[32] = { 
-  0x03,			// screen color
+  0x01,			// screen color
 
-  0x11,0x30,0x27,0x0,	// background palette 0
-  0x1c,0x20,0x2c,0x0,	// background palette 1
-  0x00,0x10,0x20,0x0,	// background palette 2
-  0x06,0x16,0x26,0x0,   // background palette 3
+  0x11,0x30,0x27,0x00,	// background palette 0
+  0x1C,0x20,0x2C,0x00,	// background palette 1
+  0x00,0x10,0x20,0x00,	// background palette 2
+  0x06,0x16,0x26,0x00,   // background palette 3
 
-  0x16,0x35,0x24,0x0,	// sprite palette 0
-  0x00,0x37,0x25,0x0,	// sprite palette 1
-  0x0d,0x2d,0x3a,0x0,	// sprite palette 2
-  0x0d,0x27,0x2a	// sprite palette 3
+  0x16,0x35,0x24,0x00,	// sprite palette 0
+  0x00,0x37,0x25,0x00,	// sprite palette 1
+  0x0D,0x2D,0x3A,0x00,	// sprite palette 2
+  0x0D,0x27,0x2A	// sprite palette 3
 };
+
+#define BYTES_PER_INSTRUCTION	2
+#define INSTRUCTIONS_PER_BLOCK  16
+#define NUMBER_OF_BLOCKS        16
+
+#define MEM_BYTES (BYTES_PER_INSTRUCTION * INSTRUCTIONS_PER_BLOCK * NUMBER_OF_BLOCKS)
+
+static unsigned char program_memory[MEM_BYTES];
+static unsigned char program_block_flags[NUMBER_OF_BLOCKS];
+
+
+
 
 // setup PPU and tables
 void setup_graphics() {
@@ -45,15 +57,25 @@ void setup_graphics() {
   pal_all(PALETTE);
 }
 
-void main(void)
+void reset_memory()
 {
+  short i;
+  for (i=0;i<MEM_BYTES;i++) {
+    program_memory[i] = 0;
+  }
+  
+  for (i=0;i<NUMBER_OF_BLOCKS;i++) {
+  	program_block_flags[i]=0;
+  }
+}
+
+void main(void)
+{  
   setup_graphics();
-  // draw message  
-  vram_adr(NTADR_A(2,2));
-  vram_write("ARCASM", 6);
   // enable rendering
   ppu_on_all();
   // infinite loop
+  reset_memory();
   while(1) {
   }
 }
